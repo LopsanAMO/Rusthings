d4e7ca743d3867c11dbb6df7b9f89a6bb5805a58
LopsanAMO <makiki789@gmail.com>
2017-12-16 07:36:26 -0600

    .

--- plus_minus/target/debug/plus_minus-16c3f92b678c77ce.d
+++
@@ -1,1 +0,0 @@
-/Users/lopsanantoniomolinaosoio/Documents/rust/projects/plus_minus/target/debug/plus_minus-16c3f92b678c77ce.dSYM: /Users/lopsanantoniomolinaosoio/Documents/rust/projects/plus_minus/src/main.rs
--- plus_minus/target/debug/plus_minus.d
+++
@@ -1,1 +0,0 @@
-/Users/lopsanantoniomolinaosoio/Documents/rust/projects/plus_minus/target/debug/plus_minus.dSYM: /Users/lopsanantoniomolinaosoio/Documents/rust/projects/plus_minus/src/main.rs
